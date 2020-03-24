ff9eb57dd5d79a0d07d4c04127e7ba5ec63786cf
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-24 20:16:20 -0300

    Se agrega ejercicio operadores

---
+++ OPERADORES/index.php
@@ -0,0 +1,98 @@
+<?php 
+
+/* Operadores Comparacion:
+$age = '18';
+echo var_dump($age<=>18);
+
+== Igual
+echo var_dump($age==18);
+=== Identico
+echo var_dump($age===18);
+!= <> Diferente
+echo var_dump($age<>18);
+!== No identico
+echo var_dump($age!==18);
+> Mayor que
+echo var_dump($age>18);
+< Menor que
+echo var_dump($age<18);
+<= Menor o igual que
+echo var_dump($age<=18);
+>= Mayor o igual que
+echo var_dump($age>=18);
+<=> Nave espacial
+*/
+
+/* Operadores Aritméticos:
+$operando1 = 2;
+$operando2 = 2;
+
+$result = $operando1+$operando2;
+echo $result;
+
+Adiccion +
+$result = $operando1+$operando2;
+Sustraccion -
+$result = $operando1-$operando2;
+Multiplicacion *
+$result = $operando1*$operando2;
+Division /
+$result = $operando1/$operando2;
+Modulo %
+$result = $operando1-($operando2*3);
+$modulo = $operando1%$operando2;
+echo "Este es el modulo $modulo \n";
+Exponenciacion **
+$result = $operando1**$operando2;
+*/
+
+/* Operadores Asignacion:
+$a = 15;
+$b = &$a;
+
+echo $a;
+echo $b;
+
+$a = 70;
+echo $a;
+echo $b;
+*/
+
+/* Operadores Logicos:
+$attendance = 6;
+$average = 5;
+$result = ()$average >= 7 or $attendance == 6;
+echo var_dump($result);
+
+Operador y &&
+$result = ($average >= 7 && $attendance == 6);
+
+Operador O ||
+$result = ($average >= 7 || $attendance == 6);
+
+Operador not !
+$result = !($average >= 7);
+*/
+
+/* Operadores Incremento y Decremento:
+$a = 5;
+echo 'Pos Incremento '.$a++;
+echo 'Pos decremento '.$a--;
+echo 'Resultado '.$a;
+*/
+
+/* Operador Ternario, Elvis y Fusion Null:
+$age;
+echo $age??'No hay edad';
+
+(Expresion bool)?expresion:expresion;
+echo ($age>=18)?'Eres mayor de edad':'Eres menor de edad';
+?
+echo $age?:'No hay edad';
+??
+echo $age??:'No hay edad';
+*/
+
+?>
+
+
